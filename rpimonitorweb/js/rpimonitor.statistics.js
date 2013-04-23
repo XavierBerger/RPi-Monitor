3709dc6a2d46f337faeb055e6d456a3285482917
XavierBerger <xberger@netbook.(none)>
2013-04-23 23:09:50 +0200

    start implementation of new statistic model

--- rpimonitorweb/js/rpimonitor.statistics.js
+++ rpimonitorweb/js/rpimonitor.statistics.js
@@ -1,18 +1,14 @@
 $(function () {
+
   // Remove the Javascript warning
   document.getElementById("infotable").deleteRow(0);
 
   // fname and rrd_data are the global variable used by all the functions below
   fname_update();
 
-  // This function updates the Web Page with the data from the RRD archive header
-  // when a new file is selected
+  // This function updates the Web Page with the data from the RRD 
+  // archive header  when a new file is selected
   function update_fname() {
-    rrd_data=rrd_data;
-  
-    // Finally, update the file name and enable the update button
-    //document.getElementById("fname").firstChild.data=fname;
-
     var graph_opts={legend: { noColumns:4}};
     var ds_graph_opts={'Oscilator':{ color: "#ff8000", 
                                      lines: { show: true, fill: true, fillColor:"#ffff80"} },
@@ -24,33 +20,47 @@ $(function () {
     var f=new rrdFlot("mygraph",rrd_data,graph_opts,ds_graph_opts);
   }
 
-  // This is the callback function that,
-  // given a binary file object,
-  // verifies that it is a valid RRD archive
-  // and performs the update of the Web page
+  // This is the callback function that, given a binary file object,
+  // verifies that it is a valid RRD archive and performs the update 
+  // of the Web page
   function update_fname_handler1(bf) {
-      var i_rrd_data=undefined;
-      if (bf.getLength()<1) {
-        alert("File "+fname+" is empty (possibly loading failed)!");
-        return 1;
+    var i_rrd_data=undefined;
+    if (bf.getLength()<1) {
+      alert("File "+fname+" is empty (possibly loading failed)!");
+      return 1;
+    }
+    try {
+      var i_rrd_data=new RRDFile(bf);            
+    } catch(err) {
+      alert("File "+fname+" is not a valid RRD archive!\n"+err);
+    }
+    if (i_rrd_data!=undefined) {
+      rrd_data=i_rrd_data;
+      update_fname()
+    }
+      
+    // Get json to extract the list of rrd availble
+    $.ajaxSetup({ cache: false });
+    $.getJSON('/rpimonitord.status', function(data) {
+      var graphlist="<select id='selected_graph'>\n";
+      for (var i=0;i<data.section.length;i++)
+      { 
+        graphlist+="<option value='/"+data.section[i]+".rrd'>"+data.section[i]+"</option>\n";
       }
-      try {
-        var i_rrd_data=new RRDFile(bf);            
-      } catch(err) {
-        alert("File "+fname+" is not a valid RRD archive!\n"+err);
-      }
-      if (i_rrd_data!=undefined) {
-        rrd_data=i_rrd_data;
-        update_fname()
-      }
+      graphlist+="</select>\n";
+      
+      //graphlist+=$("#mygraph_res").parent().html();
+      //$("#mygraph_res").parent().append().html(graphlist);
+
+    });
   }
 
   // this function is invoked when the RRD file name changes
   function fname_update() {
-    /* invalidate them, so we know when they are both loaded */
+    // invalidate them, so we know when they are both loaded
     rrd_data=undefined;
 
-    fname="/rpimonitord.rrd";
+    fname="/cpuload.rrd";
     try {
       FetchBinaryURLAsync(fname,update_fname_handler1);
     } catch (err) {
