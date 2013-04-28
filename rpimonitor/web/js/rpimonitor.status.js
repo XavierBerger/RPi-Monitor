c81738e848e5132a7dcf43cbd3b46638ddc21737
XavierBerger <xberger@netbook.(none)>
2013-04-28 12:01:17 +0200

    Do not print or render undefined values

--- rpimonitor/web/js/rpimonitor.status.js
+++ rpimonitor/web/js/rpimonitor.status.js
@@ -11,12 +11,12 @@ $(function () {
     $('#uptimeText').html("<b>" + hour+"</b> hours <b>" + minutes + "</b> minutes <b>" + seconds + "</b> seconds");
 
     // temperature
-    $('#versionText').html(
-      "<p>Distribtion: <b>" + data.distribution + "</b></p>"+
-      "<p>Kernel version: <b>" + data.kernel_version + "</b></p>"+
-      "<p>Firmvare version: <b>" + data.firmware_version + "</b></p>"+
-      "<p>Revision: <b>" + data.revision + "</b></p>"
-    );
+    versionText="";
+    if ( data.distribution ) {  versionText+="<p>Distribtion: <b>" + data.distribution + "</b></p>"}
+    if ( data.kernel_version ) {  versionText+="<p>Kernel version: <b>" + data.kernel_version + "</b></p>"}
+    if ( data.firmware_version ) {  versionText+="<p>Firmvare version: <b>" + data.firmware_version + "</b></p>"}
+    if ( data.revision ) {  versionText+="<p>Revision: <b>" + data.revision + "</b></p>"}
+    $('#versionText').html( versionText );
 
     // memory
     mempercent=100*(data.memory_total-data.memory_free)/data.memory_total;
@@ -27,17 +27,12 @@ $(function () {
       "<div class='progress progress-striped active'><div class='bar' style='width: "+mempercent+"%;'></div></div>");
 
     // cpu
-    $('#cpuText').html(
-      "<p>Load: 1 Min: <b>" + data.load1 +
-      "</b> - 5 Min: <b>" + data.load5 +
-      "</b> - 15 Min: <b>" + data.load15 +
-      "</b>"+
-      "<p>CPU frequency: <b>"+
-      (data.cpu_frequency / 1000)+
-      "MHz</b> Voltage: <b>"+
-      (data.voltage  / 1000000)+
-      "V</b></p>"
-    )
+    cpuText=""
+    cpuText+="<p>Load: 1 Min: <b>" + data.load1 + "</b> - 5 Min: <b>" + data.load5 +"</b> - 15 Min: <b>" + data.load15 + "</b>"
+    cpuText+="<p>CPU frequency: <b>"+ (data.cpu_frequency / 1000)+"MHz</b> "
+    if ( data.voltage ) { cpuText+="Voltage: <b>" +(data.voltage  / 1000000)+"V</b>" }
+    cpuText+="</p>"
+    $('#cpuText').html( cpuText );
 
     // temperature
     $('#tempText').html(
@@ -45,15 +40,32 @@ $(function () {
     );
 
     // swap
-    swappercent=100*(data.swap_total-data.swap_free)/data.swap_total;
-    $('#swapText').html(
-      "<p>Used: <b>"+Math.round((data.swap_total-data.swap_free)/ 1024)+"MB ("+Math.round(swappercent)+"%)</b>"+
-      " Free: <b>"+Math.round(data.swap_free/ 1024)+"MB</b>"+
-      " Total: <b>"+Math.round(data.swap_total/ 1024)+"MB</b></p>"+
-      "<div class='progress progress-striped active'><div class='bar' style='width: "+swappercent+"%;'></div></div>"
-    );
+    if ( data.swap_total ) {
+      swappercent=100*(data.swap_total-data.swap_free)/data.swap_total;
+      $('#swapText').html(
+        "<p>Used: <b>"+Math.round((data.swap_total-data.swap_free)/ 1024)+"MB ("+Math.round(swappercent)+"%)</b>"+
+        " Free: <b>"+Math.round(data.swap_free/ 1024)+"MB</b>"+
+        " Total: <b>"+Math.round(data.swap_total/ 1024)+"MB</b></p>"+
+        "<div class='progress progress-striped active'><div class='bar' style='width: "+swappercent+"%;'></div></div>"
+      );
+    }
 
     // sd
+    var diskText=""
+    var disk_name=["/home","/","/boot"];
+    var disk_total=[data.sdcard_home_total,data.sdcard_root_total,data.sdcard_boot_total];
+    var disk_free=[data.sdcard_home_free,data.sdcard_root_free,data.sdcard_boot_free];
+    for (iloop=0;iloop<disk_total.length;iloop++){
+      if ( disk_total ){
+        percent=100*(disk_total[iloop]-disk_free[iloop])/disk_total[iloop];
+        diskText+="<p><b>"+disk_name[iloop]+"</b> used: <b>"+Math.round((disk_total[iloop]-disk_free[iloop])/ 1024)+"MB ("+Math.round(percent)+"%)</b>"
+        diskText+=" Free: <b>"+Math.round(disk_free[iloop]/ 1024)+"MB</b>"
+        diskText+=" Total: <b>"+Math.round(disk_total[iloop]/ 1024)+"MB</b></p>"
+        diskText+="<div class='progress progress-striped active'><div class='bar' style='width: "+percent+"%;'></div></div>"
+      }
+    }
+    $('#sdText').html( diskText );
+/*
     homepercent=100*(data.sdcard_home_total-data.sdcard_home_free)/data.sdcard_home_total;
     rootpercent=100*(data.sdcard_root_total-data.sdcard_root_free)/data.sdcard_root_total;
     bootpercent=100*(data.sdcard_boot_total-data.sdcard_boot_free)/data.sdcard_boot_total;
@@ -71,7 +83,7 @@ $(function () {
       " Total: <b>"+Math.round(data.sdcard_home_total/ 1024)+"MB</b></p>"+
       "<div class='progress progress-striped active'><div class='bar' style='width: "+homepercent+"%;'></div></div>"
     );
-
+*/
     // network
     $('#netText').html(
       "Ethernet Sent: <b>" +
