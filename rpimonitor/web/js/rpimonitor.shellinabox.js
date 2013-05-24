6a0ff388af5a57e5aa4e897006302290033c2dfa
Xavier Berger <berger.xavier@gmail.com>
2013-05-24 19:52:09 +0200

    Add warning when about to close or refresh shellinabox page

--- rpimonitor/web/js/rpimonitor.shellinabox.js
+++ rpimonitor/web/js/rpimonitor.shellinabox.js
@@ -4,3 +4,13 @@ $(function () {
     ShowFriends(data.friends);
   });
 });
+
+window.onbeforeunload = function (e) {
+  if ( shellinaboxwarning ) return;
+  e = e || window.event;
+  message="Closing or refreshing this page will also close your connection.";
+  if (e) {
+    e.returnValue = message;
+  }
+  return message;
+};
