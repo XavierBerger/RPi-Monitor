bc7b74ecd3791b99d21e5c5c991fefc4aeb21a9d
Xavier Berger <berger.xavier@gmail.com>
2013-07-16 23:47:25 +0200

    Update/add copyright notice

--- rpimonitor/web/js/rpimonitor.statistics.js
+++ rpimonitor/web/js/rpimonitor.statistics.js
@@ -1,4 +1,19 @@
-//Load data from localStorage
+// This file is part of RPi-Monitor project
+//
+// Copyright 2013 - Xavier Berger - http://rpi-experiences.blogspot.fr/
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 var activestat;
 var graphconf;
 var pageid;
@@ -6,16 +21,16 @@ var static;
 
 function Start() {
   $.getJSON('static.json', function (data) {
-	      try {
-		            static = data;
-			        }
-	          catch(e){
-			        static = null;
-				    }
-		    })  .fail(function () {
-			        $('#message').html("<b>Can not get information (static.json) from RPi-Monitor server.</b>");
-				    $('#message').removeClass('hide');
-				      });
+        try {
+                static = data;
+              }
+            catch(e){
+              static = null;
+            }
+        })  .fail(function () {
+              $('#message').html("<b>Can not get information (static.json) from RPi-Monitor server.</b>");
+            $('#message').removeClass('hide');
+              });
       
   $.getJSON('statistics.json', function (data) {
     localStorage.setItem('graphconf', JSON.stringify(data));
