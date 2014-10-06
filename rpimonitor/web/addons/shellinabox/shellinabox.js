5f7de34487c056210beb2ab441022ba66364cca7
Xavier Berger <berger.xavier@gmail.com>
2014-10-06 20:28:43 +0200

    Restore shellinabox url customisation

--- rpimonitor/web/addons/shellinabox/shellinabox.js
+++ rpimonitor/web/addons/shellinabox/shellinabox.js
@@ -1,4 +1,5 @@
 var shellinaboxwarning = false;
+var shellinaboxuri;
 
 $(function () {
   $('#pagetitle').remove();
@@ -9,11 +10,26 @@ $(function () {
             '<p>'+
               '<b>Shellinabox</b><br>'+
               '<form class="form-inline">'+
+                'Shellinabox url: '+
+                '<div class="input-group">'+
+                  '<input type="text" class="form-control" id="shellinaboxuri">'+
+                  '<span class="input-group-btn">'+
+                    '<button id="defaulturi" class="btn" type="button">Default</button>'+
+                  '</span>'+
+                '</div><br>'+
                 '<input type="checkbox" id="shellinaboxwarning"> Show warning on page close or refresh'+
               '</form>'+
             '</p>'
   $(options).insertBefore("#configurationInsertionPoint")
   
+  
+  $("#defaulturi").click(function(){
+    shellinaboxuri = "/shellinabox";
+    $('#shellinaboxuri').val(shellinaboxuri);
+    localStorage.setItem('shellinaboxuri', shellinaboxuri);
+    $("#shellinaboxframe").attr('src', shellinaboxuri);
+  });
+
   shellinaboxwarning=(localStorage.getItem('shellinaboxwarning') === 'true' );
   $('#shellinaboxwarning').attr('checked', shellinaboxwarning );
   
@@ -21,6 +37,16 @@ $(function () {
     shellinaboxwarning = $('#shellinaboxwarning').is(":checked");
     localStorage.setItem('shellinaboxwarning', shellinaboxwarning);
   });
+
+  shellinaboxuri=(localStorage.getItem('shellinaboxuri') || '/shellinabox');
+  $('#shellinaboxuri').val(shellinaboxuri);
+  $('#shellinaboxuri').keyup(function(){
+    shellinaboxuri = $('#shellinaboxuri').val();
+    localStorage.setItem('shellinaboxuri', shellinaboxuri);
+    $("#shellinaboxframe").attr('src', shellinaboxuri);
+  });
+  
+  $("#shellinaboxframe").attr('src', shellinaboxuri);
 });
 
 function resize_frame(){
