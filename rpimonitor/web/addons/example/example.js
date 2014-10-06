5cf958a6c5f66d0aa8c4452b2ca9bebaf0a6db80
Xavier Berger <berger.xavier@gmail.com>
2014-10-06 20:29:12 +0200

    Add addons example option

--- rpimonitor/web/addons/example/example.js
+++ rpimonitor/web/addons/example/example.js
@@ -1,5 +1,14 @@
 $(function () {
-  //alert('Example addons javascript')
+  alert('Example addons javascript')
+  
+  options = '<hr>'+
+            '<p>'+
+              '<b>Example</b><br>'+
+              '<form class="form-inline">'+
+                '<input type="checkbox" id="shellinaboxwarning"> Add addons option(s)'+
+              '</form>'+
+            '</p>'
+  $(options).insertBefore("#configurationInsertionPoint")
 });
 
 
