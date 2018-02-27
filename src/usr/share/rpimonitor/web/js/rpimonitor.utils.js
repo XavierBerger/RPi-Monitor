036a05bb3a4600433bdaf2834748d5c46e63d422
Xavier Berger <berger.xavier@gmail.com>
2018-02-27 10:10:44 +0100

    Add LinkToGraph function

--- src/usr/share/rpimonitor/web/js/rpimonitor.utils.js
+++ src/usr/share/rpimonitor/web/js/rpimonitor.utils.js
@@ -175,4 +175,7 @@ function InsertHTML( url ){
     return result
 }
 
+function LinkToGraph(page, graph, text){
+    return "<a target=_blank href=statistics.html?activePage="+page+"&graph="+graph+">"+text+"</a>"
+}
 
